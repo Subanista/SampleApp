39690d49aa3572c218ee37c98cfd5c3864ed82c9
Subanista <subanista14@gmail.com>
2024-11-02 23:34:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
+import React from 'react';
+import Navbar from './NavBar';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-      
-        <div>
+        <h1>Welcome to My App</h1>
+        <div className="form-container">
           <form>
-            <p>Hello,
-              Update your Info </p>
-            <input type="text" name="Name" placeholder="Enter name" required></input>
-            <input type="number" name="Age" placeholder="Enter your age" required ></input>
-            <input type="text" name="Address" placeholder="Enter Address" required></input>
-            <input type="text" name="Pno" placeholder="Enter Phone num" required></input>
-            <button type="submit"> Save Info </button>
+            <p>Update your Info</p>
+            <input type="text" name="Name" placeholder="Enter name" required />
+            <input type="number" name="Age" placeholder="Enter your age" required />
+            <input type="text" name="Address" placeholder="Enter Address" required />
+            <input type="text" name="Pno" placeholder="Enter Phone num" required />
+            <button type="submit">Save Info</button>
           </form>
-
         </div>
       </header>
+      <Footer />
     </div>
   );
 }
